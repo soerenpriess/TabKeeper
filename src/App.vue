497857ff39dbb55c299a1d9b124727a80fc5f75a
Sören Prieß <soeren.priess@codeanker.de>
2025-05-25 01:23:26 +0200

    add loading screen for app

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,36 @@
 <template>
-  <div v-if="!loading" class="min-h-[600px] min-w-[300px] bg-slate-800 font-sans">
-    <div class="bg-slate-900 px-4 py-3">
-       <h1 class="text-xl font-bold mb-2 text-center text-white">Tab Keeper</h1>
-       <div class="w-full h-8 bg-slate-700 border-slate-600 border-1 rounded-lg flex justify-evenly items-center relative ">
-         <div
-           class="absolute bg-slate-900 rounded-md h-[calc(100%-0.2rem)] transition-all duration-300 ease-in-out border-slate-600 border-1"
-           :style="{
-             width: `calc(50% - 0.20rem)`,
-             left: activeTab === 'home' ? '0.25rem' : 'calc(50%)'
-           }"
-         ></div>
-         <div
-           v-for="tab in tabs"
-           :key="tab.id"
-           @click="activeTab = tab.id"
-           class="w-full h-full flex justify-center items-center relative z-10 cursor-pointer"
-         >
-           <p class="text-white">{{ tab.label }}</p>
-         </div>
-       </div>
-     </div>
-     <div class="p-4">
-      <TabSetList
-      :tabSets="tabSets"
-      @save-set="saveSet"
-      @delete-set="deleteSet"
-      @open-tabs="openTabs"
-      @delete-tab="deleteTab"
-      @update-set="updateSet"
-    />
-
-    <Modal
-      v-if="modalVisible"
-      title="Confirm Deletion"
-      @confirm="deleteSetConfirm"
-      @cancel="modalVisible = false"
-    >
-      <p>Do you really want to delete the Tab Set <strong>{{ deletingSet?.name }}</strong>?</p>
-    </Modal>
-     </div>
+  <div class="min-h-[600px] min-w-[300px] bg-slate-800 font-sans">
+    <template v-if="!loading">
+      <div class="bg-slate-900 px-4 py-3">
+        <h1 class="text-xl font-bold mb-2 text-center text-white">Tab Keeper</h1>
+        <div
+          class="w-full h-8 bg-slate-700 border-slate-600 border-1 rounded-lg flex justify-evenly items-center relative ">
+          <div
+            class="absolute bg-slate-900 rounded-md h-[calc(100%-0.2rem)] transition-all duration-300 ease-in-out border-slate-600 border-1"
+            :style="{
+              width: `calc(50% - 0.20rem)`,
+              left: activeTab === 'home' ? '0.25rem' : 'calc(50%)'
+            }"></div>
+          <div v-for="tab in tabs" :key="tab.id" @click="activeTab = tab.id"
+            class="w-full h-full flex justify-center items-center relative z-10 cursor-pointer">
+            <p class="text-white">{{ tab.label }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4">
+        <TabSetList :tabSets="tabSets" @save-set="saveSet" @delete-set="deleteSet" @open-tabs="openTabs"
+          @delete-tab="deleteTab" @update-set="updateSet" />
+  
+        <Modal v-if="modalVisible" title="Confirm Deletion" @confirm="deleteSetConfirm" @cancel="modalVisible = false">
+          <p>Do you really want to delete the Tab Set <strong>{{ deletingSet?.name }}</strong>?</p>
+        </Modal>
+      </div>
+    </template>
+    <template v-else>
+      <div class="flex flex-col items-center justify-center h-[600px]">
+        <FontAwesomeIcon :icon="faSpinner" class="animate-spin text-white text-3xl" />
+      </div>
+    </template>
   </div>
 </template>
 
@@ -47,6 +39,8 @@ import TabSetList from './components/TabSetList.vue'
 import { onMounted, ref } from 'vue'
 import { useTabSets } from './composables/useTabSets'
 import Modal from './components/Modal.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const { tabSets, loadTabSets, saveTabSet, removeTabSet } = useTabSets()
 const loading = ref(true)
@@ -62,7 +56,9 @@ const activeTab = ref('home')
 onMounted(async () => {
   loading.value = true
   await loadTabSets()
-  loading.value = false
+  setTimeout(() => {
+    loading.value = false
+  }, 200)
 })
 
 function saveSet(set) {
@@ -90,12 +86,9 @@ function deleteSetConfirm() {
 }
 
 function deleteTab(tabTitle) {
-  console.log('Deleting tab:', tabTitle)
   const setToUpdate = tabSets.value.find(set => set.tabs.some(tab => tab.title === tabTitle))
-  console.log('Set to update:', setToUpdate)
   if (setToUpdate) {
     setToUpdate.tabs = setToUpdate.tabs.filter(tab => tab.title !== tabTitle)
-    console.log('Updated set:', setToUpdate)
     if (setToUpdate.tabs.length === 0) {
       removeTabSet(setToUpdate.id)
     } else {
